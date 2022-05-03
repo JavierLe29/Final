2859f61f548e33582f57dc8412b520d2173f81e6
LesterGL <lestergarcia690@gmail.com>
2022-05-03 09:53:22 -0600

    Cree el pequeño programa del animal

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Ingresa tu animal favorito")
+animal=input()
+if animal=='cerdo':
+    print("A mi tambien me gustan los cerdos")
+else:
+    print("Es un animal interesante pero me gustan mas los cerdos")
